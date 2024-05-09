2503abb733bc831a0aee621a7320d2173dadeee1
moseffect21 <moseffect21@gmail.com>
2024-05-09 15:15:38 +0300

    add meta

--- src/application/presentation/pages/LinksPage/LinksPage.tsx
+++ src/application/presentation/pages/LinksPage/LinksPage.tsx
@@ -6,44 +6,48 @@ import LinksContainer from './components/LinksContainer'
 import { linksData, partnersData } from './linksPageData'
 import PartnerItem from './components/PartnerItem'
 import Link from 'next/link'
+import { MetaData } from 'application/presentation/meta/MetaContent'
 
 export default function LinksPage() {
   return (
-    <div className={s.container}>
-      <div className={s.content}>
-        <div className={s.img_container}>
-          <Image src={vladPng} alt="" />
-        </div>
-        <div className={s.title}>Влад Носачев</div>
-        <div className={s.subtitle}>ОКОЛОКАЛЬЯНА</div>
-        <div className={s.separator}></div>
-
-        <LinksContainer title="ОКОЛОКАЛЬЯНА везде" data={linksData.all} />
-        <div className={s.separator}></div>
-
-        <LinksContainer title="Личные проекты автора" data={linksData.personal} />
-        <div className={s.separator}></div>
-
-        <LinksContainer
-          title="Вы всегда можете поддержать автора и проект"
-          data={linksData.support}
-        />
-        <div className={s.separator}></div>
-
-        <div className={s.partners_block}>
-          <div className={s.title}>ТОП бренды индустрии, респект за поддержку.</div>
-          <div className={s.partners_container}>
-            {partnersData.map(item => (
-              <PartnerItem key={`partner_${item.id}`} data={item} />
-            ))}
+    <>
+      <MetaData title={'Околокальяна - ссылки'} desc="" />
+      <div className={s.container}>
+        <div className={s.content}>
+          <div className={s.img_container}>
+            <Image src={vladPng} alt="" />
           </div>
+          <div className={s.title}>Влад Носачев</div>
+          <div className={s.subtitle}>ОКОЛОКАЛЬЯНА</div>
+          <div className={s.separator}></div>
+
+          <LinksContainer title="ОКОЛОКАЛЬЯНА везде" data={linksData.all} />
+          <div className={s.separator}></div>
+
+          <LinksContainer title="Личные проекты автора" data={linksData.personal} />
+          <div className={s.separator}></div>
+
+          <LinksContainer
+            title="Вы всегда можете поддержать автора и проект"
+            data={linksData.support}
+          />
+          <div className={s.separator}></div>
+
+          <div className={s.partners_block}>
+            <div className={s.title}>ТОП бренды индустрии, респект за поддержку.</div>
+            <div className={s.partners_container}>
+              {partnersData.map(item => (
+                <PartnerItem key={`partner_${item.id}`} data={item} />
+              ))}
+            </div>
+          </div>
+          <Link href="https://t.me/okolomanager" className={s.manager_link}>
+            Менеджер по всем вопросам
+            <br />
+            <span>@okolomanager</span>
+          </Link>
         </div>
-        <Link href="https://t.me/okolomanager" className={s.manager_link}>
-          Менеджер по всем вопросам
-          <br />
-          <span>@okolomanager</span>
-        </Link>
       </div>
-    </div>
+    </>
   )
 }
