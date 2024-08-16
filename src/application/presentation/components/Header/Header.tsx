987f3504d8ccc4cc2bd24b67c4c42af85ec782c0
moseffect21 <moseffect21@gmail.com>
2024-08-16 17:13:13 +0300

    add mobile header

--- src/application/presentation/components/Header/Header.tsx
+++ src/application/presentation/components/Header/Header.tsx
@@ -1,25 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LogoSvg from 'images/logo.svg'
 import Login from 'images/icons/login.svg'
+import Burger from 'images/icons/burger_icon.svg'
+import Close from 'images/icons/close_icon.svg'
 import s from './Header.module.scss'
 import NavBar from '../NavBar'
 import Link from 'next/link'
 import Button from '../uiComponents/Button'
+import { DesktopContainer, MobileContainer } from '../uiComponents/AdaptiveContainers'
+import cn from 'classnames'
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState<boolean>()
+  const burgerClickHandler = () => setShowMenu(prev => !prev)
+  useEffect(() => {
+    document.body.className = showMenu ? 'modal_opened' : ''
+  }, [showMenu])
   return (
     <>
-      <NavBar />
-      <header className={s.header}>
-        <div className={s.headerLeft}></div>
-        <Link className={s.logo} href="/">
-          <LogoSvg />
-        </Link>
-        <Button className={s.loginBtn} containerClassName={s.btnContainer}>
-          <Login />
-          Войти
-        </Button>
-      </header>
+      <DesktopContainer>
+        <NavBar />
+        <header className={s.header}>
+          <div className={s.headerLeft}></div>
+          <Link className={s.logo} href="/">
+            <LogoSvg />
+          </Link>
+          <Button className={s.loginBtn} containerClassName={s.btnContainer}>
+            <Login />
+            Войти
+          </Button>
+        </header>
+      </DesktopContainer>
+      <MobileContainer>
+        <header className={s.header}>
+          <div className={s.headerLeft} onClick={burgerClickHandler}>
+            {showMenu ? <Close /> : <Burger />}
+          </div>
+          <Link className={s.logo} href="/">
+            <LogoSvg />
+          </Link>
+          <div className={s.headerRight}>
+            <Login />
+          </div>
+        </header>
+        <div className={cn(s.mobileMenu, { [s.opened]: showMenu })}>
+          <NavBar />
+        </div>
+      </MobileContainer>
     </>
   )
 }
--- src/application/presentation/components/NavBar/NavBar.tsx
+++ src/application/presentation/components/NavBar/NavBar.tsx
@@ -3,12 +3,15 @@ import s from './NavBar.module.scss'
 import SearchField from './components/SearchField'
 import NavList from './components/NavList'
 import SocialLinks from './components/SocialLinks'
+import { DesktopContainer } from '../uiComponents/AdaptiveContainers'
 
 export default function NavBar() {
   return (
     <div className={s.container}>
       <div className={s.wrapper}>
-        <SearchField />
+        <DesktopContainer>
+          <SearchField />
+        </DesktopContainer>
         <NavList />
         <SocialLinks />
       </div>
