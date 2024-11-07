4276fc08b48dbfeeff4f4428eda1da1c95bafa13
moseffect21 <moseffect21@gmail.com>
2024-11-07 15:20:53 +0300

    fix

--- src/application/data/api/search.ts
+++ src/application/data/api/search.ts
@@ -1,7 +1,12 @@
 import { Article } from 'application/domain/entities/article/Article'
 import apiClient from '../apiClient'
 
+const controller = new AbortController()
+
 export const fetchSearchRequest = async (value: string): Promise<Article[]> => {
-  const { data } = await apiClient.get(`/api/v1/search?text=${value}`)
+  controller.abort()
+  const { data } = await apiClient.get(`/api/v1/search?text=${value}`, {
+    signal: controller.signal,
+  })
   return data
 }
